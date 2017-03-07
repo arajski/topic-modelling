e6192732037d1b5e2f22b98867c9d1cd7b068102
Rjsk <artur.rajski@hotmail.com>
2017-03-07 13:24:22 +0000

    Loading files from HDFS

--- src/main/scala/TopicModelling.scala
+++ src/main/scala/TopicModelling.scala
@@ -1,26 +1,25 @@
-// import required spark classes
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import scala.util.parsing.json._
 
- 
-// define main method (scala entry point)
 object TopicModelling {
-  def main(args: Array[String]) {
- 
-    // initialise spark context
-    val conf = new SparkConf().setAppName("TopicModelling")
-    val sc = new SparkContext(conf)
+
+    def main(args: Array[String]) {
+
+        // initialise spark context
+        val conf = new SparkConf().setAppName("TopicModelling")
+        val sc = new SparkContext(conf)
+        val sparkSession = SparkSession.builder.getOrCreate()
+        val df = sparkSession.read.json("hdfs://localhost:9000/hdfs/data/*")
+        
+        //selecting only text element of tweets
+        val tweetTexts = df.select("text")
+        
+        // terminate spark context
+        sc.stop()
     
-    val textFile = sc.textFile("hdfs://localhost:9000/hdfs/data/Oct21.txt")
-    val counts = textFile.flatMap(line => line.split(" "))
-                                   
-    println("RESULT:")
-    println(counts.collect().mkString(", ")) 
-    println("=========")
-    // terminate spark context
-    sc.stop()
-    
-  }
+    }
 }
 
